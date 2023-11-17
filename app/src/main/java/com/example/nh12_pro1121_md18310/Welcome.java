51ca0e15d1e7dff328c4605417de7a06fdcf69cc
24082004 <loindph36082@fpl.edu.vn>
2023-11-17 17:27:48 +0700

    update code

---
+++ app/src/main/java/com/example/nh12_pro1121_md18310/Welcome.java
@@ -0,0 +1,25 @@
+package com.example.nh12_pro1121_md18310;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Welcome.this, DangNhapActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
